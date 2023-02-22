bab99cfb97827170b001a8b61392404f8900d6b0
Hossein <hosseinfti@gmail.com>
2023-02-22 10:29:07 +0330

    Changed: regenerating the card type

--- src/config/types.tsx
+++ src/config/types.tsx
@@ -1,16 +1,15 @@
 export interface BankCardType {
-  title: string;
-  description: string;
+  title?: string;
+  description?: string;
   detail: string;
   id: number;
   image: string;
 }
-export interface cardComponentType {
+export interface cardComponentType
+  extends Pick<BankCardType, "title" | "description"> {
   className?: string;
   loading?: boolean;
   onClick?: () => void;
   detail?: React.ReactNode;
-  description?: string;
-  title?: string;
   image?: React.ReactNode;
 }
